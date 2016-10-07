615555f080f5cbfe757182b359a02177fbf75976
Victor Parmar <victor.parmar@canoo.com>
2016-10-07 22:37:17 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # mongoose-field-encryption
-A simple symmetric encryption plugin for fields with string values 
+
+A simple symmetric encryption plugin for individual fields. The goal of this plugin is to encrypt data but still allow searching over the fields. This plugin relies on the Node `crypto` module. Encryption and decryption happen transparently during save and find. 
+
+At present this plugin only works on fields with string values. Also consider [mongoose-encryption](https://github.com/joegoldbeck/mongoose-encryption) if you have other requirements.
+
+## How it works
+
+Encryption is performed using `AES-256-CTR`. To encrypt, the relevant fields are encrypted with the provided secret and the resulting hex string is put in place of the actual value. An extra `boolean` field with the prefix `__enc_` is added to the document which indicates if the provided field is encrypted or not.
+
+## Requirements
+
+- Node `>=4.4.7`
+- MongoDB `>=2.6.10`
+
+## Installation
+
+`npm install mongoose-field-encryption`
+
+## Usage
+
+Keep your secret a secret. Ideally it should only live as an environment variable and 
+
+For example, given a schema as follows:
+```javascript
+let mongoose                = require('mongoose');
+let mongooseFieldEncryption = require('mongoose-field-encryption');
+let Schema                  = mongoose.Schema;
+
+let Post = new Schema({
+  title: String, 
+  message: String
+});
+
+Post.plugin(mongooseFieldEncyption, {fields: ['message'], secret: 'some secret key'});
+```
+
+The resulting documents will have the following format:
+```javascript
+{
+  _id: ObjectId,
+  title: String,
+  message: String, // encrypted hex value as string
+  __enc_message: true // boolean marking if the field is encrypted or not
+}
+```
+
+`find` works transparently and you can make new documents as normal, but you should not use the `lean` option on a find if you want the fields of the document to be decrypted. `findOne`, `findById`, as well as `save` and `update` also all work as normal. From the mongoose documentation: _Note that findAndUpdate/Remove do not execute any hooks or validation before making the change in the database. If you need hooks and validation, first query for the document and then save it._
+
+Also note that if you manually set the value `__enc_` prefix field to true then the encryption is not run on the corresponding field and this may result in the plaintext value being stored in the db.
+
+### Static methods
+
+For performance reasons, once the document has been encrypted, it remains so. The following methods are thus added to the schema:
+- `encryptFieldsSync()`: synchronous call that encrypts all fields as given by the plugin options
+- `decryptFieldsSync()`: synchronous call that decrypts encrypted fields as given by the plugin options
+
+## Testing
+
+0. Install dependencies with `npm install` and [install mongo](http://docs.mongodb.org/manual/installation/) if you don't have it yet.
+1. Start mongo with `mongod`.
+2. Run tests with `npm test`. Additionally you can pass your own mongodb uri as an environment variable if you would like to test against your own database, for e.g. `URI='mongodb://username:password@localhost/mongoose-field-encryption-test' npm test`
+
+
+## TODO
+
+- add support for nested fields
+
---
+++ test/test-mongoose-field-encryption.js
@@ -0,0 +1,241 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+const Promise = require('bluebird');
+const mongoose = require('mongoose');
+mongoose.Promise = Promise;
+
+const fieldEncryptionPlugin = require('../lib/mongoose-field-encryption');
+
+const uri = process.env.URI || 'mongodb://localhost/mongoose-field-encryption-test';
+
+describe('mongoose-field-encryption', () => {
+
+  let FieldEncryptionSchema = new mongoose.Schema({
+    toEncrypt1: { type: String, required: true },
+    toEncrypt2: { type: String, required: true }
+  });
+
+  FieldEncryptionSchema.plugin(
+    fieldEncryptionPlugin, {
+      fields: ['toEncrypt1', 'toEncrypt2'],
+      secret: 'letsdothis' // should ideally be process.env.SECRET
+    }
+  );
+
+  let FieldEncryption = mongoose.model('FieldEncryption', FieldEncryptionSchema);
+
+  before(() => {
+    return mongoose.connect(uri)
+      .then(() => {
+        return mongoose.connection.db.dropDatabase();
+      });
+  });
+
+  after(() => {
+    mongoose.connection.close()
+  });
+
+  describe('static methods', () => {
+    it('should encrypt fields', () => {
+
+      // given
+      let sut = new FieldEncryption({
+        toEncrypt1: 'some stuff',
+        toEncrypt2: 'should be hidden'
+      });
+
+      // when
+      sut.encryptFieldsSync();
+
+      // then
+      expect(sut.__enc_toEncrypt1).to.be.true;
+      expect(sut.toEncrypt1).to.equal('b27d5768b82263ece8bd');
+      expect(sut.__enc_toEncrypt2).to.be.true;
+      expect(sut.toEncrypt2).to.equal('b27a5578f43537fbebfb2e365ab13977');
+
+    });
+
+    it('should not encrypt already encrypted fields', () => {
+
+      // given
+      let sut = new FieldEncryption({
+        toEncrypt1: 'some stuff',
+        toEncrypt2: 'should be hidden'
+      });
+
+      // when
+      sut.encryptFieldsSync();
+      sut.encryptFieldsSync();
+
+      // then
+      expect(sut.__enc_toEncrypt1).to.be.true;
+      expect(sut.toEncrypt1).to.equal('b27d5768b82263ece8bd');
+      expect(sut.__enc_toEncrypt2).to.be.true;
+      expect(sut.toEncrypt2).to.equal('b27a5578f43537fbebfb2e365ab13977');
+
+    });
+
+    it('should decrypt fields', () => {
+
+      // given
+      let sut = new FieldEncryption({
+        toEncrypt1: 'b27d5768b82263ece8bd',
+        __enc_toEncrypt1: true,
+        toEncrypt2: 'b27a5578f43537fbebfb2e365ab13977',
+        __enc_toEncrypt2: true
+      });
+
+      // when
+      sut.decryptFieldsSync();
+
+      // then
+      expect(sut.__enc_toEncrypt1).to.be.false;
+      expect(sut.toEncrypt1).to.equal('some stuff');
+      expect(sut.__enc_toEncrypt2).to.be.false;
+      expect(sut.toEncrypt2).to.equal('should be hidden');
+
+    });
+
+    it('should ignore multiple decrypt field calls', () => {
+
+      // given
+      let sut = new FieldEncryption({
+        toEncrypt1: 'b27d5768b82263ece8bd',
+        __enc_toEncrypt1: true,
+        toEncrypt2: 'b27a5578f43537fbebfb2e365ab13977',
+        __enc_toEncrypt2: true
+      });
+
+      // when
+      sut.decryptFieldsSync();
+      sut.decryptFieldsSync();
+
+      // then
+      expect(sut.__enc_toEncrypt1).to.be.false;
+      expect(sut.toEncrypt1).to.equal('some stuff');
+      expect(sut.__enc_toEncrypt2).to.be.false;
+      expect(sut.toEncrypt2).to.equal('should be hidden');
+
+    });
+
+  });
+
+  describe('database', () => {
+    it('should not encrypt non-string fields', done => {
+
+      // given
+      let NestedFieldEncryptionSchema = new mongoose.Schema({
+        toEncrypt: {
+          nested: { type: String, required: true }
+        }
+      });
+
+      NestedFieldEncryptionSchema.plugin(fieldEncryptionPlugin, { fields: ['toEncrypt'], secret: 'icanhazcheezburger' });
+
+      let NestedFieldEncryption = mongoose.model('NestedFieldEncryption', NestedFieldEncryptionSchema);
+
+      let sut = new NestedFieldEncryption({
+        toEncrypt: {
+          nested: 'some stuff to encrypt'
+        }
+      });
+
+      // when
+      sut.save()
+        .then(() => {
+          done(new Error('should not have saved with a non-string field'));
+        })
+        .catch(err => {
+          // then
+          done();
+        });
+    });
+
+    it('should encrypt fields on save and decrypt fields on findOne', () => {
+
+      // given
+      let sut = new FieldEncryption({
+        toEncrypt1: 'some stuff',
+        toEncrypt2: 'should be hidden'
+      });
+
+      // when
+      return sut.save()
+        .then(() => {
+          expect(sut.__enc_toEncrypt1).to.be.true;
+          expect(sut.toEncrypt1).to.equal('b27d5768b82263ece8bd');
+          expect(sut.__enc_toEncrypt2).to.be.true;
+          expect(sut.toEncrypt2).to.equal('b27a5578f43537fbebfb2e365ab13977');
+
+          return FieldEncryption.findOne({ _id: sut._id });
+        })
+        .then(found => {
+          expect(found.__enc_toEncrypt1).to.be.false;
+          expect(found.toEncrypt1).to.equal('some stuff');
+          expect(found.__enc_toEncrypt2).to.be.false;
+          expect(found.toEncrypt2).to.equal('should be hidden');
+        });
+    });
+
+    it('should store encrypted fields as plaintext on findOneAndUpdate', () => {
+
+      // given
+      let sut = new FieldEncryption({
+        toEncrypt1: 'some stuff',
+        toEncrypt2: 'should be hidden'
+      });
+
+      // when
+      return sut.save()
+        .then(() => {
+          expect(sut.__enc_toEncrypt1).to.be.true;
+          expect(sut.toEncrypt1).to.equal('b27d5768b82263ece8bd');
+          expect(sut.__enc_toEncrypt2).to.be.true;
+          expect(sut.toEncrypt2).to.equal('b27a5578f43537fbebfb2e365ab13977');
+
+          return FieldEncryption.findOneAndUpdate({ _id: sut._id }, { $set: { toEncrypt1: 'snoop', __enc_toEncrypt1: false } }, { new: true });
+        })
+        .then(found => {
+          // then
+          expect(found.__enc_toEncrypt1).to.be.false;
+          expect(found.toEncrypt1).to.equal('snoop');
+          expect(found.__enc_toEncrypt2).to.be.false;
+          expect(found.toEncrypt2).to.equal('should be hidden');
+        });
+    });
+
+    it('should encrypt fields on update', () => {
+
+      // given
+      let sut = new FieldEncryption({
+        toEncrypt1: 'some stuff',
+        toEncrypt2: 'should be hidden'
+      });
+
+      // when
+      return sut.save()
+        .then(() => {
+          expect(sut.__enc_toEncrypt1).to.be.true;
+          expect(sut.toEncrypt1).to.equal('b27d5768b82263ece8bd');
+          expect(sut.__enc_toEncrypt2).to.be.true;
+          expect(sut.toEncrypt2).to.equal('b27a5578f43537fbebfb2e365ab13977');
+
+          return FieldEncryption.update({ _id: sut._id }, { $set: { toEncrypt1: 'snoop', __enc_toEncrypt1: false } });
+        })
+        .then(() => {
+          return FieldEncryption.findById(sut._id);
+        })
+        .then(found => {
+          // then
+          expect(found.__enc_toEncrypt1).to.be.false;
+          expect(found.toEncrypt1).to.equal('snoop');
+          expect(found.__enc_toEncrypt2).to.be.false;
+          expect(found.toEncrypt2).to.equal('should be hidden');
+        });
+    });
+
+  });
+
+});
