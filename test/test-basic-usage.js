f13bc35bd5d0d4cd84c45151b4ece425f9eb6649
Victor Parmar <vic@smalldata.tech>
2019-05-31 11:02:51 +0200

    F add usage test for text search over encrypted fields

--- test/test-basic-usage.js
+++ test/test-basic-usage.js
@@ -1,7 +1,10 @@
 "use strict";
 
+const crypto = require("crypto");
 const expect = require("chai").expect;
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
 mongoose.set("bufferCommands", false);
 
 const mongooseFieldEncryption = require("../lib/mongoose-field-encryption").fieldEncryption;
@@ -18,10 +21,7 @@ describe("basic usage", function() {
   });
 
   it("should save a document", function(done) {
-    // var conn = mongoose.connection;
-
-    const Schema = mongoose.Schema;
-
+    // given
     const postSchema = new Schema({
       title: String,
       message: String
@@ -31,7 +31,10 @@ describe("basic usage", function() {
 
     const Post = mongoose.model("Post", postSchema);
     const post = new Post({ title: "some text", message: "hello all" });
+
+    // when
     post.save(function(err) {
+      // then
       if (err) {
         return done(err);
       }
@@ -47,4 +50,58 @@ describe("basic usage", function() {
       done();
     });
   });
+
+  it("should search for a document on an encrypted field", async function() {
+    // given
+    const messageSchema = new Schema({
+      title: String,
+      message: String,
+      name: String
+    });
+
+    messageSchema.plugin(mongooseFieldEncryption, {
+      fields: ["message", "name"],
+      secret: "some secret key",
+      saltGenerator: function(secret) {
+        return "1234567890123456";
+      }
+    });
+
+    const title = "some text";
+    const name = random(10);
+    const message = "hello all";
+
+    const Message = mongoose.model("Message", messageSchema);
+
+    const messageToSave = new Message({ title, message, name });
+    await messageToSave.save();
+
+    const messageToSearchWith = new Message({ title: "", message: "", name });
+    messageToSearchWith.encryptFieldsSync();
+
+    // when
+    const results = await Message.find({ name: messageToSearchWith.name });
+
+    // then
+    expect(results.length).to.equal(1);
+    const ret = results[0].toObject();
+
+    expect(ret.title).to.equal(title);
+    expect(ret.message).to.equal(message);
+    expect(ret.name).to.equal(name);
+  });
 });
+
+function random(howMany, chars) {
+  chars = chars || "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+  const rnd = crypto.randomBytes(howMany);
+  const value = new Array(howMany);
+  const len = Math.min(256, chars.length);
+  const d = 256 / len;
+
+  for (var i = 0; i < howMany; i++) {
+    value[i] = chars[Math.floor(rnd[i] / d)];
+  }
+
+  return value.join("");
+}
